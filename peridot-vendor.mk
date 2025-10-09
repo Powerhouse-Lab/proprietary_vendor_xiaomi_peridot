f6953cef59d2164e5de048e132741d874e9b0132
AdarshGrewal <adarshgrewal@gmail.com>
2025-10-09 21:00:25 +0530

    peridot: Use correct fw for nfc
    Change-Id: I838a96f19c3e8bae26952eb5da4f3f0aefa51551

--- peridot-vendor.mk
+++ peridot-vendor.mk
@@ -1573,7 +1573,7 @@ PRODUCT_PACKAGES += \
     libskewknob \
     libslimclient \
     libsmemlog \
-    libsn100u_fw \
+    libsn220u_fw \
     libsnapdragoncolor-manager \
     libsnapdragoncolor-qdcm \
     libsns_device_mode_stub \

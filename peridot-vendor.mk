8dfe19282d85361c8d8c4ae41f939ce0327c0271
zenin1504 <kayozzprojects@yandex.com>
2025-12-26 19:35:54 +0100

    sync

--- peridot-vendor.mk
+++ peridot-vendor.mk
@@ -1177,7 +1177,6 @@ PRODUCT_PACKAGES += \
     libcneoplookup \
     libcneqmiutils \
     libcodec2_soft_ac4dec \
-    libcodec2_soft_common_prebuilt \
     libcodec2_soft_ddpdec \
     libcodec2_store_dolby \
     libconfigdb \

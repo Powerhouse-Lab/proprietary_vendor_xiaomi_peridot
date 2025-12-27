b00df610c496b2a6ef653b5a8404cd9e66652dbd
zenin1504 <kayozzprojects@yandex.com>
2025-12-27 12:33:53 +0000

    peridot: Use prebuilt libcodec2_soft_common.so for Dolby
    Change-Id: I6f932cbc21f54edd830c6781aa0cfe5d
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- peridot-vendor.mk
+++ peridot-vendor.mk
@@ -1179,6 +1179,7 @@ PRODUCT_PACKAGES += \
     libcneoplookup \
     libcneqmiutils \
     libcodec2_soft_ac4dec \
+    libcodec2_soft_common_prebuilt \
     libcodec2_soft_ddpdec \
     libcodec2_store_dolby \
     libconfigdb \
